02e44b07d580bdb7bc9ee1b9bb1df0dcb1ac9e61
darkuknigh <monish13570@gmail.com>
2025-07-31 23:44:17 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 8000;
 require("dotenv").config();
+const port = process.env.PORT;
+
+console.log(port);
 
 app.set("view engine", "ejs");
 
@@ -14,5 +16,5 @@ const home = require("./home.js");
 app.get("/", home.render);
 
 app.listen(port, () => {
-    console.log(`app running on port:${port}`);
+    console.log(`app running`);
 });
